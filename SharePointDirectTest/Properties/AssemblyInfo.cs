3ab63895c717a8ee220b2c0421198ef00efc2d59
exxdaw@outlook.com <exxdaw@outlook.com>
2018-09-04 23:25:16 +0800

    new branch which includes unit tests

---
+++ SharePointDirectTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SharePointDirectTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SharePointDirectTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5c20a389-3814-4188-9989-6d0f4e91f5e7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SharePointDirectTest/UnitTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SharePointDirectTest
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void GetNumberOfItems_NoURL_ShouldThrowException()
+        {
+            
+        }
+    }
+}
